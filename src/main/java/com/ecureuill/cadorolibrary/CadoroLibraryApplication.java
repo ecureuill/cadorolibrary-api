6718e39979928fd68a3983c8834ca470869a2961
Camilla <logikasciuro@gmail.com>
2023-10-03 20:24:01 +0000

    🎉 begin project

---
+++ src/main/java/com/ecureuill/cadorolibrary/CadoroLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.ecureuill.cadorolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadoroLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadoroLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecureuill/cadorolibrary/CadoroLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecureuill.cadorolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadoroLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
